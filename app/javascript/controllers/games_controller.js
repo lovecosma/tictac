21a1adc1202fa3c2bba9ded41f4186a53161a53c
Matthew Williams <williamsmatthew333@gmail.com>
2025-07-28 20:34:22 -0600

    adds winning status

--- app/javascript/controllers/games_controller.js
+++ app/javascript/controllers/games_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
- static targets = ['cell']
+ static targets = ['cell', 'status', 'new']
 
   connect() {
     this.board = ['', '', '', '', '', '', '', '', ''];
@@ -24,47 +24,44 @@ export default class extends Controller {
       const index = parseInt(e.target.dataset.index);
       this.board[index] = this.currentPlayer;
       this.winner = this.checkWinner();
-        if(this.winner){
-            // this.celebrate()
-            // alert(`Player ${this.winner} wins!`);
-        } else if(this.draw()){
-            alert('It\'s a draw!');
-        } else {
-            this.nextTurn();
+      if(this.winner) {
+        this.showGameOverStatus();
+      }else if(this.draw()){
+        alert('It\'s a draw!');
+      } else {
+        this.nextTurn();
       }
-
     }
   }
 
-    // celebrate() {
-    //  const jsConfetti = new JSConfetti()
-    //  jsConfetti.addConfetti()
-    // }
+  checkWinner() {
+    const winningCombo = this.winningCombos.find(combo =>
+        combo.every(index => this.board[index] === 'X') ||
+        combo.every(index => this.board[index] === 'O')
+    );
 
-    checkWinner() {
-        const winningCombo = this.winningCombos.find(combo =>
-            combo.every(index => this.board[index] === 'X') ||
-            combo.every(index => this.board[index] === 'O')
-        );
-
-        if (winningCombo) {
-            this.showWinner(winningCombo);
-            return this.board[winningCombo[0]];
-        }
-        return null;
+    if (winningCombo) {
+        this.showWinner(winningCombo);
+        return this.board[winningCombo[0]];
     }
+    return null;
+}
 
-    showWinner(combo) {
-        combo.forEach(index => {
-            this.cellTargets[index].style.backgroundColor = 'rgb(187, 247, 208)';
-        })
-    }
+  showWinner(combo) {
+    combo.forEach(index => {
+        this.cellTargets[index].style.backgroundColor = 'rgb(187, 247, 208)';
+    })
+ }
 
-   nextTurn(){
+  nextTurn(){
     this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
   }
 
-   draw(){
-    return this.board.every(cell => cell !== '');
+  draw(){
+   return this.board.every(cell => cell !== '');
+  }
+
+  showGameOverStatus(){
+    this.statusTarget.innerText = `Game Over! ${this.winner} wins!`;
   }
 }
